0474bdc9b0536ef78a19d902d21323eaad50be64
ravibkjoshi <ravibkjoshi@gmail.com>
2017-03-25 19:28:41 -0700

    Snapshot for pull

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,22 +3,22 @@ class Challenge < ActiveRecord::Base
   has_many :completed_challenges
   validates :name, presence: :true
 
-  def average
-  	completed_challenges = self.completed_challenges
- 	challenge_time_total = completed_challenges.reduce(|accum, c| accum + c.time)
- 	challenge_time_average = challenge_time_total/completed_challenges.length 
+ #  def average
+ #  	completed_challenges = self.completed_challenges
+ # 	challenge_time_total = completed_challenges.reduce(|accum, c| accum + c.time)
+ # 	challenge_time_average = challenge_time_total/completed_challenges.length 
 
-  end
+ #  end
 
-  def median
+ #  def median
 
-  	midpoint = array.length/2
-  	if array.length.even?
-  		median = (array[midpoint] + array[midpoint - 1])/2
-	else
-  		median = array[midpoint]
-  	end
-  end
+ #  	midpoint = array.length/2
+ #  	if array.length.even?
+ #  		median = (array[midpoint] + array[midpoint - 1])/2
+	# else
+ #  		median = array[midpoint]
+ #  	end
+ #  end
 
   def fork_url
   	url = self.url
@@ -28,4 +28,5 @@ class Challenge < ActiveRecord::Base
   def pull_url
   	url = self.url
   	pull_url = url[0..7] + "api." + url[8..18] + "repos/" + url[19..-1] + "/pulls"
+  end
 end
